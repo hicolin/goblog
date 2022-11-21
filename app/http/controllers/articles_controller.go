1a1f96cc22317d388bab08d92c9482be1a1feea1
ccl <dev@dev.com>
2022-11-21 10:31:43 +0800

    文章内容页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -29,15 +29,21 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "500 服务器内部错误")
 		}
 	} else {
+		viewDir := "resources/views"
+
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
+		logger.LogError(err)
+
+		newFiles := append(files, viewDir+"/articles/show.gohtml")
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL":  route.Name2URL,
 				"Uint64ToString": types.Uint64ToString,
 			}).
-			ParseFiles("resources/views/articles/show.gohtml")
-
+			ParseFiles(newFiles...)
 		logger.LogError(err)
-		err = tmpl.Execute(w, article)
+
+		err = tmpl.ExecuteTemplate(w, "app", article)
 		logger.LogError(err)
 	}
 }
