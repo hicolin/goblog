774b65a26443f43a82c9d860b53adf40c7364add
ccl <dev@dev.com>
2022-11-24 17:37:14 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"goblog/app/http/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -14,22 +15,29 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
-	_user.Create()
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID为"+_user.GetStringID())
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
 	}
 
 	// 表单不通过，重新显示表单
--- app/http/models/user/user.go
+++ app/http/models/user/user.go
@@ -5,7 +5,9 @@ import "goblog/app/http/models"
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"colum:name;type:varchar(255);not null;unique"`
-	Email    string `gorm:"colum:email;type:varchar(255);default:NULL;unique"`
-	Password string `gorm:"colum:password;type:varchar(255)"`
+	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);unique" valid:"email"`
+	Password string `gorm:"type:varchar(255)" valid:"password"`
+
+	PasswordConfirm string `gorm:"-" valid:"password_confirm"`
 }
